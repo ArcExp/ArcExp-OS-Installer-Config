6ebeb569af2f9b16c2f8a41a320f97b1f3426c24
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-07-15 02:01:26 +0100

    Add files via upload

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,10 +1,11 @@
+
 #!/usr/bin/env bash
 
 # Loop until pacman-init.service finishes
 printf 'Waiting for pacman-init.service to finish running before starting the installation... '
 
 while true; do
-    systemctl status pacman-init.service | grep -q 'Finished Initializing Pacman keyring.'
+    systemctl status pacman-init.service | grep -q 'Finished Initializes Pacman keyring.'
 
     if [[ $? -eq 0 ]]; then
         printf 'Done'
@@ -19,6 +20,6 @@ sudo pacman -Syy
 
 # Optimize download speed using reflector based on IP address
 country_code=$(curl -s https://ipapi.co/country/)
-sudo reflector --country $country_code --age 10 --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country $country_code --age 20 --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 exit 0
