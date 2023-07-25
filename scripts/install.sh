c5a45aa2357adcb5c91140e665fdb25ee54dbe0b
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-07-25 20:39:35 +0100

    Add files via upload

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
 declare -r workdir='/mnt'
-declare -r cache_dir="/var/cache/pacman/pkg"
-
-# Function to check if the device is an NVMe SSD
-is_nvme_ssd() {
-    local dev_name="${1##*/}"
-    if [[ -L "/sys/block/$dev_name" ]]; then
-        # Check if it's an NVMe device by checking the subsystem path
-        local subsystem_path=$(readlink -f "/sys/block/$dev_name/device/subsystem")
-        if [[ "$subsystem_path" == *"/nvme/"* ]]; then
-            return 0
-        fi
-    fi
-    return 1
-}
 
 # Function to display an error and exit
 show_error() {
@@ -38,19 +24,15 @@ if mountpoint -q "$workdir"; then
     show_error "$workdir is already a mountpoint, unmount this directory and try again"
 fi
 
-# Determine the partition path and partition table type
-if is_nvme_ssd "$OSI_DEVICE_PATH"; then
-    # For NVMe SSD
-    declare -r partition_path="${OSI_DEVICE_PATH}p"
-    declare -r partition_table="gpt"
+# Determine the partition table type and boot partition number
+partition_table=$(lsblk -no "PARTTYPENAME" "${OSI_DEVICE_PATH}")
+
+if [[ $partition_table == "gpt" ]]; then
+    declare -r boot_partition_number=2
+elif [[ $partition_table == "dos" ]]; then
+    declare -r boot_partition_number=1
 else
-    # For other devices
-    declare -r partition_path="${OSI_DEVICE_PATH}"
-    if [[ ! -d "$workdir/sys/firmware/efi" ]]; then
-        declare -r partition_table="msdos"
-    else
-        declare -r partition_table="gpt"
-    fi
+    show_error "Unknown partition table type: $partition_table"
 fi
 
 # Write partition table to the disk
@@ -58,29 +40,19 @@ if [ "${OSI_DEVICE_IS_PARTITION}" -eq 0 ]; then
     # Disk-level partitioning
     sudo parted "${OSI_DEVICE_PATH}" mklabel "$partition_table" --script || show_error "Failed to create partition table on $OSI_DEVICE_PATH"
 
-    if is_nvme_ssd "$OSI_DEVICE_PATH"; then
-        # GPT partitioning for NVMe SSD
-        create_partition() {
-            sudo parted "$OSI_DEVICE_PATH" mkpart primary "$1" "$2" --script || show_error "Failed to create $1 partition on $OSI_DEVICE_PATH"
-        }
+    # Create partitions
+    create_partition() {
+        sudo parted "$OSI_DEVICE_PATH" mkpart primary "$1" "$2" --script || show_error "Failed to create $1 partition on $OSI_DEVICE_PATH"
+    }
 
+    if [[ $partition_table == "dos" ]]; then
         create_partition fat32 1MiB 1GB
-        sudo parted "$OSI_DEVICE_PATH" set 1 esp on || show_error "Failed to set boot flag on /boot/efi partition"
-        sudo parted "$OSI_DEVICE_PATH" set 1 boot on || show_error "Failed to set boot flag on /boot/efi partition"
-        create_partition btrfs 1GB 100%
+        sudo parted "$OSI_DEVICE_PATH" set $boot_partition_number boot on || show_error "Failed to set boot flag on /boot partition"
     else
-        # MBR partitioning for BIOS systems on physical hardware
-        create_partition() {
-            sudo parted "$OSI_DEVICE_PATH" mkpart primary "$1" "$2" 100% --script || show_error "Failed to create $1 partition on $OSI_DEVICE_PATH"
-            sudo parted "$OSI_DEVICE_PATH" set 1 boot on || show_error "Failed to set boot flag on /boot partition"
-        }
-
-        if [[ "$partition_table" == "gpt" ]]; then
-            create_partition btrfs 1MiB
-        else
-            create_partition btrfs 1MiB
-        fi
+        create_partition fat32 1MiB 513MiB
+        sudo parted "$OSI_DEVICE_PATH" set $boot_partition_number esp on || show_error "Failed to set ESP flag on /boot/efi partition"
     fi
+    create_partition btrfs 513MiB 100%
 fi
 
 # Check if encryption is requested, write filesystems accordingly
@@ -88,47 +60,32 @@ if [[ "$OSI_USE_ENCRYPTION" -eq 1 ]]; then
     # If user requested disk encryption
     if [[ "$OSI_DEVICE_IS_PARTITION" -eq 0 ]]; then
         # If target is a drive
-        printf '%s\n' "$OSI_ENCRYPTION_PIN" | sudo cryptsetup -q luksFormat "${partition_path}2" || show_error "Failed to format ${partition_path}2 with LUKS"
-        printf '%s\n' "$OSI_ENCRYPTION_PIN" | sudo cryptsetup open "${partition_path}2" "$rootlabel" - || show_error "Failed to open ${partition_path}2 with LUKS"
+        printf '%s\n' "$OSI_ENCRYPTION_PIN" | sudo cryptsetup -q luksFormat "${OSI_DEVICE_PATH}${boot_partition_number}" || show_error "Failed to format ${OSI_DEVICE_PATH}${boot_partition_number} with LUKS"
+        printf '%s\n' "$OSI_ENCRYPTION_PIN" | sudo cryptsetup open "${OSI_DEVICE_PATH}${boot_partition_number}" "$rootlabel" - || show_error "Failed to open ${OSI_DEVICE_PATH}${boot_partition_number} with LUKS"
         sudo mkfs.btrfs -f "/dev/mapper/$rootlabel" || show_error "Failed to create Btrfs filesystem on /dev/mapper/$rootlabel"
     else
         # If target is a partition
-        sudo mkfs.btrfs -f "$partition_path" || show_error "Failed to create Btrfs filesystem on $partition_path"
+        sudo mkfs.btrfs -f "${OSI_DEVICE_PATH}${boot_partition_number}" || show_error "Failed to create Btrfs filesystem on ${OSI_DEVICE_PATH}${boot_partition_number}"
     fi
 else
     # If no disk encryption requested
     if [[ "$OSI_DEVICE_IS_PARTITION" -eq 0 ]]; then
         # If target is a drive
-        yes | sudo mkfs.btrfs -f "$partition_path" || show_error "Failed to create Btrfs filesystem on $partition_path"
+        yes | sudo mkfs.btrfs -f "${OSI_DEVICE_PATH}${boot_partition_number}" || show_error "Failed to create Btrfs filesystem on ${OSI_DEVICE_PATH}${boot_partition_number}"
     else
         # If target is a partition
-        yes | sudo mkfs.btrfs -f "$partition_path" || show_error "Failed to create Btrfs filesystem on $partition_path"
+        yes | sudo mkfs.btrfs -f "${OSI_DEVICE_PATH}${boot_partition_number}" || show_error "Failed to create Btrfs filesystem on ${OSI_DEVICE_PATH}${boot_partition_number}"
     fi
 fi
 
 # Mount the root partition
 sudo mkdir -p "$workdir" || show_error "Failed to create mount directory $workdir"
+sudo mount "${OSI_DEVICE_PATH}${boot_partition_number}" "$workdir" || show_error "Failed to mount ${OSI_DEVICE_PATH}${boot_partition_number} to $workdir"
 
-if is_nvme_ssd "$OSI_DEVICE_PATH"; then
-    # For NVMe SSD
-    if [[ ! -d "$workdir/sys/firmware/efi" ]]; then
-        # Non-EFI system
-        sudo mount "${partition_path}2" "$workdir" || show_error "Failed to mount ${partition_path}2 to $workdir"
-    else
-        # EFI system
-        sudo mount "${OSI_DEVICE_EFI_PARTITION}" "$workdir/boot/efi" || show_error "Failed to mount EFI partition to $workdir/boot/efi"
-        sudo mount "${partition_path}2" "$workdir" || show_error "Failed to mount ${partition_path}2 to $workdir"
-    fi
-else
-    # For other devices
-    if [[ ! -d "$workdir/sys/firmware/efi" ]]; then
-        # Non-EFI system
-        sudo mount "$partition_path" "$workdir" || show_error "Failed to mount $partition_path to $workdir"
-    else
-        # EFI system
-        sudo mount "${OSI_DEVICE_EFI_PARTITION}" "$workdir/boot/efi" || show_error "Failed to mount EFI partition to $workdir/boot/efi"
-        sudo mount "${partition_path}2" "$workdir" || show_error "Failed to mount ${partition_path}2 to $workdir"
-    fi
+# Mount the EFI partition for UEFI systems
+if [[ $partition_table == "gpt" ]]; then
+    sudo mkdir -p "$workdir/boot/efi" || show_error "Failed to create directory $workdir/boot/efi"
+    sudo mount "$OSI_DEVICE_EFI_PARTITION" "$workdir/boot/efi" || show_error "Failed to mount EFI partition to $workdir/boot/efi"
 fi
 
 # Install system packages
